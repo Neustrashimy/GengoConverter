ed0f79681f667d4f7f2a3859bdce8ee7780f075c
Neustrashimy <shilka409+github@gmail.com>
2017-06-10 11:14:16 +0900

    辞書ファイル追記

---
+++ _gcData.php
@@ -0,0 +1,76 @@
+<?php
+
+// GengoConverter Database File
+
+$_gengoData = array(
+
+	// 飛鳥時代, Asuka Era
+	array("Kanji"=>"大化",	"ja"=>array("たいか"),									"en"=>array("Taika"),							"from"=>"645/7/17",	"to"=>"650/3/22",	"emperor"=>array("孝徳天皇")),
+	array("Kanji"=>"白雉",	"ja"=>array("はくち"),									"en"=>array("Hakuchi"),							"from"=>"650/3/22",	"to"=>"654/11/24",	"emperor"=>array("孝徳天皇")),
+	array("Kanji"=>"白雉",	"ja"=>array("はくち"),									"en"=>array("Hakuchi"),							"from"=>"654/11/24","to"=>"686/8/14",	"emperor"=>array("")),
+	array("Kanji"=>"朱鳥",	"ja"=>array("しゅちょう", "すちょう", "あかみどり"),	"en"=>array("Shucho", "Sucho", "Akamidori"),	"from"=>"686/8/14", "to"=>"686/10/1",	"emperor"=>array("天武天皇")),
+	array("Kanji"=>"朱鳥",	"ja"=>array("しゅちょう", "すちょう", "あかみどり"),	"en"=>array("Shucho", "Sucho", "Akamidori"),	"from"=>"686/10/1", "to"=>"701/5/3",	"emperor"=>array("")),
+	array("Kanji"=>"大宝",	"ja"=>array("たいほう", "だいほう"),					"en"=>array("Taiho", "Daiho"),					"from"=>"701/5/3",	"to"=>"704/6/16",	"emperor"=>array("文武天皇")),
+	array("Kanji"=>"慶雲",	"ja"=>array("けいうん", "きょううん"),					"en"=>array("Keiun", "Kyouun"),					"from"=>"704/6/16",	"to"=>"708/2/7",	"emperor"=>array("文武天皇","元明天皇")),
+	array("Kanji"=>"和銅",	"ja"=>array("わどう"),									"en"=>array("Wado"),							"from"=>"708/2/7",	"to"=>"715/10/3",	"emperor"=>array("元明天皇")),
+
+	// 奈良時代, Nara Era
+	array("Kanji"=>"霊亀",		"ja"=>array("れいき"),				"en"=>array("Reiki"),			"from"=>"715/10/3",	"to"=>"717/12/24",	"emperor"=>array("元正天皇")),
+	array("Kanji"=>"養老",		"ja"=>array("ようろう"),			"en"=>array("Yourou"),			"from"=>"717/12/24","to"=>"724/3/3",	"emperor"=>array("元正天皇")),
+	array("Kanji"=>"神亀",		"ja"=>array("じんき"),				"en"=>array("Jinki"),			"from"=>"724/3/3",	"to"=>"729/9/2",	"emperor"=>array("聖武天皇")),
+	array("Kanji"=>"天平感宝",	"ja"=>array("てんぴょうかんぽう"),	"en"=>array("Tenpyou-Kanpo"),	"from"=>"729/9/2",	"to"=>"749/8/19",	"emperor"=>array("聖武天皇")),
+	array("Kanji"=>"天平勝宝",	"ja"=>array("てんぴょうしょうぽう"),"en"=>array("Tenpyou-Shouhou"),	"from"=>"749/8/19",	"to"=>"757/9/6",	"emperor"=>array("孝謙天皇")),
+	array("Kanji"=>"天平宝字",	"ja"=>array("てんぴょうほうじ"),	"en"=>array("Tenpyou-Houji"),	"from"=>"757/9/6",	"to"=>"765/2/1",	"emperor"=>array("孝謙天皇", "淳仁天皇", "称徳天皇")),
+	array("Kanji"=>"天平神護",	"ja"=>array("てんぴょうじんご"),	"en"=>array("Tenpyou-Jingo"),	"from"=>"765/2/1",	"to"=>"767/9/13",	"emperor"=>array("称徳天皇")),
+	array("Kanji"=>"神護景雲",	"ja"=>array("じんごけいうん"),		"en"=>array("Jingo-Keiun"),		"from"=>"767/9/13",	"to"=>"770/10/23",	"emperor"=>array("称徳天皇")),
+	array("Kanji"=>"宝亀",		"ja"=>array("ほうき"),				"en"=>array("Houki"),			"from"=>"770/10/23","to"=>"781/1/30",	"emperor"=>array("光仁天皇")),
+	array("Kanji"=>"天応",		"ja"=>array("てんおう", "てんのう"),"en"=>array("Tenou", "Tennou"),	"from"=>"781/1/30",	"to"=>"782/9/30",	"emperor"=>array("光仁天皇", "桓武天皇")),
+	array("Kanji"=>"延暦",		"ja"=>array("えんりゃく"),			"en"=>array("Enryaku"),			"from"=>"782/9/30",	"to"=>"806/6/8",	"emperor"=>array("桓武天皇")),
+	
+	// 平安時代, Heian Era
+	array("Kanji"=>"大同",	"ja"=>array("だいどう"),							"en"=>array("Daido"),						"from"=>"806/6/8",	"to"=>"810/10/20",	"emperor"=>array("平城天皇", "嵯峨天皇")),
+	array("Kanji"=>"弘仁",	"ja"=>array("こうにん"),							"en"=>array("Kounin"),						"from"=>"810/10/20","to"=>"824/2/8",	"emperor"=>array("嵯峨天皇", "淳和天皇")),
+	array("Kanji"=>"天長",	"ja"=>array("てんちょう"),							"en"=>array("Tencho"),						"from"=>"824/2/8",	"to"=>"834/2/14",	"emperor"=>array("淳和天皇", "仁明天皇")),
+	array("Kanji"=>"承和",	"ja"=>array("じょうわ", "しょうわ"),				"en"=>array("Jyowa", "Showa"),				"from"=>"834/2/14",	"to"=>"848/7/16",	"emperor"=>array("仁明天皇")),
+	array("Kanji"=>"嘉祥",	"ja"=>array("かしょう", "かじょう"),				"en"=>array("Kasho", "Kajo"),				"from"=>"848/7/16",	"to"=>"851/6/1",	"emperor"=>array("仁明天皇", "文徳天皇")),
+	array("Kanji"=>"仁寿",	"ja"=>array("にんじゅ"),							"en"=>array("Ninju"),						"from"=>"851/6/1",	"to"=>"854/12/23",	"emperor"=>array("文徳天皇")),
+	array("Kanji"=>"斉衡",	"ja"=>array("さいこう"),							"en"=>array("Saiko"),						"from"=>"854/12/23","to"=>"857/3/20",	"emperor"=>array("文徳天皇")),
+	array("Kanji"=>"天安",	"ja"=>array("てんあん", "てんなん"),				"en"=>array("Ten-an", "Ten-nan"),			"from"=>"857/3/20",	"to"=>"859/5/20",	"emperor"=>array("文徳天皇", "清和天皇")),
+	array("Kanji"=>"貞観",	"ja"=>array("じょうがん"),							"en"=>array("Jogan"),						"from"=>"859/5/20",	"to"=>"877/6/1",	"emperor"=>array("清和天皇", "陽成天皇")),
+	array("Kanji"=>"元慶",	"ja"=>array("がんぎょう"),							"en"=>array("Gangyo"),						"from"=>"877/6/1",	"to"=>"885/3/11",	"emperor"=>array("陽成天皇", "光孝天皇")),
+	array("Kanji"=>"仁和",	"ja"=>array("にんな", "にんわ"),					"en"=>array("Ninna", "Ninwa"),				"from"=>"885/3/11",	"to"=>"889/5/30",	"emperor"=>array("光孝天皇", "宇多天皇")),
+	array("Kanji"=>"寛平",	"ja"=>array("かんぴょう", "かんぺい", "かんへい"),	"en"=>array("Kanpyo", "Kanpei", "Kanhei"),	"from"=>"889/5/30",	"to"=>"898/5/20",	"emperor"=>array("宇多天皇", "醍醐天皇")),
+	array("Kanji"=>"昌泰",	"ja"=>array("しょうたい"),							"en"=>array("Shotai"),						"from"=>"898/5/20",	"to"=>"901/8/31",	"emperor"=>array("醍醐天皇")),
+	array("Kanji"=>"延喜",	"ja"=>array("えんぎ"),								"en"=>array("Engi"),						"from"=>"901/8/31",	"to"=>"923/5/29",	"emperor"=>array("醍醐天皇")),
+	array("Kanji"=>"延長",	"ja"=>array("えんちょう"),							"en"=>array("Enchou"),						"from"=>"923/5/29",	"to"=>"931/5/16",	"emperor"=>array("醍醐天皇", "朱雀天皇")),
+	array("Kanji"=>"承平",	"ja"=>array("じょうへい", "しょうへい"),			"en"=>array("Jyohei", "Shohei"),			"from"=>"931/5/16",	"to"=>"938/6/22",	"emperor"=>array("朱雀天皇")),
+	array("Kanji"=>"天慶",	"ja"=>array("てんぎょう", "てんきょう"),			"en"=>array("Tengyou", "Tenkyou"),			"from"=>"938/6/22",	"to"=>"947/5/15",	"emperor"=>array("朱雀天皇", "村上天皇")),
+	array("Kanji"=>"天暦",	"ja"=>array("てんりゃく"),							"en"=>array("Tenryaku"),					"from"=>"947/5/15",	"to"=>"957/11/21",	"emperor"=>array("村上天皇")),
+	array("Kanji"=>"天徳",	"ja"=>array("てんとく"),							"en"=>array("Tentoku"),						"from"=>"957/11/21","to"=>"961/3/5",	"emperor"=>array("村上天皇")),
+	array("Kanji"=>"応和",	"ja"=>array("おうわ"),								"en"=>array("Ouwa"),						"from"=>"961/3/5",	"to"=>"964/8/19",	"emperor"=>array("村上天皇")),
+	array("Kanji"=>"康保",	"ja"=>array("こうほう"),							"en"=>array("Kouhou"),						"from"=>"964/8/19",	"to"=>"968/9/8",	"emperor"=>array("村上天皇", "冷泉天皇")),
+	array("Kanji"=>"安和",	"ja"=>array("あんな", "あんわ"),					"en"=>array("Anna", "Anwa"),				"from"=>"968/9/8",	"to"=>"970/5/3",	"emperor"=>array("冷泉天皇", "円融天皇")),
+	array("Kanji"=>"天禄",	"ja"=>array("てんろく"),							"en"=>array("Tenroku"),						"from"=>"970/5/3",	"to"=>"974/1/16",	"emperor"=>array("円融天皇")),
+	array("Kanji"=>"天延",	"ja"=>array("てんえん"),							"en"=>array("Tenen"),						"from"=>"974/1/16",	"to"=>"976/8/11",	"emperor"=>array("円融天皇")),
+	array("Kanji"=>"貞元",	"ja"=>array("じょうげん"),							"en"=>array("Jyogen"),						"from"=>"976/8/11",	"to"=>"978/12/31",	"emperor"=>array("円融天皇")),
+	array("Kanji"=>"天元",	"ja"=>array("てんげん"),							"en"=>array("Tengen"),						"from"=>"978/12/31","to"=>"983/5/29",	"emperor"=>array("円融天皇")),
+	array("Kanji"=>"永観",	"ja"=>array("えいかん"),							"en"=>array("Eikan"),						"from"=>"983/5/29",	"to"=>"985/5/19",	"emperor"=>array("円融天皇", "花山天皇")),
+	array("Kanji"=>"寛和",	"ja"=>array("かんな", "かんわ"),					"en"=>array("Kanna", "Kanwa"),				"from"=>"985/5/19",	"to"=>"987/5/5",	"emperor"=>array("花山天皇", "一条天皇")),
+	array("Kanji"=>"永延",	"ja"=>array("えいえん"),							"en"=>array("Eien"),						"from"=>"987/5/5",	"to"=>"989/9/10",	"emperor"=>array("一条天皇")),
+	array("Kanji"=>"永祚",	"ja"=>array("えいそ"),								"en"=>array("Eiso"),						"from"=>"989/9/10",	"to"=>"990/11/26",	"emperor"=>array("一条天皇")),
+	array("Kanji"=>"正暦",	"ja"=>array("しょうりゃく"),						"en"=>array("Syouryaku"),					"from"=>"990/11/26","to"=>"995/3/25",	"emperor"=>array("一条天皇")),
+	array("Kanji"=>"長徳",	"ja"=>array("ちょうとく"),							"en"=>array("Choutoku"),					"from"=>"995/3/25",	"to"=>"999/2/1",	"emperor"=>array("一条天皇")),
+	array("Kanji"=>"長保",	"ja"=>array("ちょうほう"),							"en"=>array("Chouhou"),						"from"=>"999/2/1",	"to"=>"1004/8/8",	"emperor"=>array("一条天皇")),
+	array("Kanji"=>"寛弘",	"ja"=>array("かんこう"),							"en"=>array("Kankou"),						"from"=>"1004/8/8",	"to"=>"1013/2/8",	"emperor"=>array("一条天皇", "三条天皇")),
+	array("Kanji"=>"長和",	"ja"=>array("ちょうわ"),							"en"=>array("Chouwa"),						"from"=>"1013/2/8",	"to"=>"1017/5/21",	"emperor"=>array("三条天皇", "後一条天皇")),
+	array("Kanji"=>"寛仁",	"ja"=>array("かんにん"),							"en"=>array("Kan-nin"),						"from"=>"1017/5/21","to"=>"1021/3/17",	"emperor"=>array("後一条天皇")),
+	array("Kanji"=>"治安",	"ja"=>array("じあん"),								"en"=>array("Jian"),						"from"=>"1021/3/17","to"=>"1024/8/19",	"emperor"=>array("後一条天皇")),
+	array("Kanji"=>"万寿",	"ja"=>array("まんじゅ"),							"en"=>array("Manju"),						"from"=>"1024/8/19","to"=>"1028/8/18",	"emperor"=>array("後一条天皇")),
+	array("Kanji"=>"長元",	"ja"=>array("ちょうげん"),							"en"=>array("Chougen"),						"from"=>"1028/8/18","to"=>"1037/5/9",	"emperor"=>array("後一条天皇", "後朱雀天皇")),
+	
+
+);
+
+
+
+var_dump($_gengoData);
